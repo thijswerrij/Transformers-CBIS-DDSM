c12d37e68d013c0c34c7320a9746a3892c0fe8e3
Thijs Werrij <thijswerrij@gmail.com>
2021-01-21 18:02:45 +0100

    Train and evaluate are now imported from VisualTransformers fork

--- ResViT-train.py
+++ ResViT-train.py
@@ -6,17 +6,15 @@ import PIL
 import time
 import torch
 import torchvision
-import torch.nn.functional as F
-from einops import rearrange
-from torch import nn
-import torch.nn.init as init
 
 from load_data import plot
 
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-from ResViT import ViTResNet, BasicBlock
+import sys
+sys.path.append('../VisualTransformers')
+from ResViT import ViTResNet, BasicBlock, train, evaluate
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #device = "cpu"
@@ -116,50 +114,6 @@ test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE_TE
 
 #%%
 
-def train(model, optimizer, data_loader, loss_history):
-    total_samples = len(data_loader.dataset)
-    model.train()
-
-    for i, (data, target) in enumerate(data_loader):
-        #print(data.dtype, target.dtype)
-        
-        data, target = data.to(device), target.to(device, dtype=torch.int64)
-        optimizer.zero_grad()
-        output = F.log_softmax(model(data), dim=1)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        
-        if i % 100 == 0:
-            print('[' +  '{:5}'.format(i * len(data)) + '/' + '{:5}'.format(total_samples) +
-                  ' (' + '{:3.0f}'.format(100 * i / len(data_loader)) + '%)]  Loss: ' +
-                  '{:6.4f}'.format(loss.item()))
-            loss_history.append(loss.item())
-            
-def evaluate(model, data_loader, loss_history):
-    model.eval()
-    
-    total_samples = len(data_loader.dataset)
-    correct_samples = 0
-    total_loss = 0
-
-    with torch.no_grad():
-        for data, target in data_loader:
-            data, target = data.to(device), target.to(device, dtype=torch.int64)
-            output = F.log_softmax(model(data), dim=1)
-            loss = F.nll_loss(output, target, reduction='sum')
-            _, pred = torch.max(output, dim=1)
-            
-            total_loss += loss.item()
-            correct_samples += pred.eq(target).sum()
-
-    avg_loss = total_loss / total_samples
-    loss_history.append(avg_loss)
-    print('\nAverage test loss: ' + '{:.4f}'.format(avg_loss) +
-          '  Accuracy:' + '{:5}'.format(correct_samples) + '/' +
-          '{:5}'.format(total_samples) + ' (' +
-          '{:4.2f}'.format(100.0 * correct_samples / total_samples) + '%)\n')
-
 N_EPOCHS = 10#50
 
 
@@ -169,6 +123,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
 #optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate,momentum=.9,weight_decay=1e-4)
 #lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[35,48],gamma = 0.1)
 
+init_time = time.time()
 train_loss_history, test_loss_history = [], []
 for epoch in range(1, N_EPOCHS + 1):
     print('Epoch:', epoch)
@@ -176,8 +131,9 @@ for epoch in range(1, N_EPOCHS + 1):
     train(model, optimizer, train_loader, train_loss_history)
     print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
     evaluate(model, test_loader, test_loss_history)
-
-print('Execution time')
+    
+minutes, seconds = divmod(time.time() - init_time, 60)
+print('Total execution time:', '{:.0f}m {:.2f}s'.format(minutes, seconds))
 
 PATH = ".\ViTRes.pt" # Use your own path
 torch.save(model.state_dict(), PATH)
