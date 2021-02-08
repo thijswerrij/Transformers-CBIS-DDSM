e804d7ca6687d58e02e791032cc1fb1773db2f94
Thijs Werrij <thijswerrij@gmail.com>
2021-02-08 19:28:56 +0100

    Added some metrics

--- ResViT_train.py
+++ ResViT_train.py
@@ -7,6 +7,7 @@ import time
 import torch
 import torchvision
 import torch.nn.functional as F
+from sklearn.metrics import confusion_matrix
 
 from load_data import plot
 
@@ -95,9 +96,11 @@ transform = torchvision.transforms.Compose([
 
 #%%
 
-def train(model, optimizer, data_loader, loss_history):
+def train(model, optimizer, data_loader, loss_history, acc_history):
     total_samples = len(data_loader.dataset)
     model.train()
+    correct_samples = 0
+    total_loss = 0
 
     for i, (data, target) in enumerate(data_loader):
         data, target = data.to(device), target.to(device, dtype=torch.int64)
@@ -106,19 +109,29 @@ def train(model, optimizer, data_loader, loss_history):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
+        
+        total_loss += loss.item()
+        _, pred = torch.max(output, dim=1)
+        correct_samples += pred.eq(target).sum().item()
 
         if i % 100 == 0:
             print('[' +  '{:5}'.format(i * len(data)) + '/' + '{:5}'.format(total_samples) +
                   ' (' + '{:3.0f}'.format(100 * i / len(data_loader)) + '%)]  Loss: ' +
                   '{:6.4f}'.format(loss.item()))
-            loss_history.append(loss.item())
+        
+    avg_loss = total_loss / total_samples
+    accuracy = correct_samples / total_samples
+    loss_history.append(avg_loss)
+    acc_history.append(accuracy)
+    return output
             
-def evaluate(model, data_loader, loss_history):
+def evaluate(model, data_loader, loss_history, acc_history, conf_matrices, binary=False):
     model.eval()
     
     total_samples = len(data_loader.dataset)
     correct_samples = 0
     total_loss = 0
+    conf_mat = 0
 
     with torch.no_grad():
         for data, target in data_loader:
@@ -128,32 +141,39 @@ def evaluate(model, data_loader, loss_history):
             _, pred = torch.max(output, dim=1)
             
             total_loss += loss.item()
-            correct_samples += pred.eq(target).sum()
+            correct_samples += pred.eq(target).sum().item()
+            
+            if not binary:
+                conf_mat = np.add(conf_mat, confusion_matrix(target.cpu(),pred.cpu(), labels=[0,1,2]))
+            else:
+                conf_mat = np.add(conf_mat, confusion_matrix(target.cpu(),pred.cpu(), labels=[0,1]))
 
     avg_loss = total_loss / total_samples
+    accuracy = correct_samples / total_samples
     loss_history.append(avg_loss)
+    acc_history.append(accuracy)
+    conf_matrices.append(conf_mat)
     print('\nAverage test loss: ' + '{:.4f}'.format(avg_loss) +
           '  Accuracy:' + '{:5}'.format(correct_samples) + '/' +
           '{:5}'.format(total_samples) + ' (' +
-          '{:4.2f}'.format(100.0 * correct_samples / total_samples) + '%)\n')
+          '{:4.2f}'.format(100.0 * accuracy) + '%)\n')
+    return output
 
 #%%
 
 if __name__ == "__main__":
 
-    BATCH_SIZE_TRAIN = 5
-    BATCH_SIZE_TEST = 5
-    batch_size = (BATCH_SIZE_TRAIN, BATCH_SIZE_TEST)
-    is_binary = True
+    batch_size = (10, 10)
+    is_binary = False
     
     #file_params = "180_cropped"
     #file_params = "400x1000_cropped"
     file_params = "scaled_0.1_cropped"
-    train_dataset = CBISDataset(f"calc_case_description_train_set_{file_params}", transform, BATCH_SIZE_TRAIN, is_binary)
-    test_dataset = CBISDataset(f"calc_case_description_test_set_{file_params}", transform, BATCH_SIZE_TEST, is_binary)
+    train_dataset = CBISDataset(f"calc_case_description_train_set_{file_params}", transform, batch_size[0], is_binary)
+    test_dataset = CBISDataset(f"calc_case_description_test_set_{file_params}", transform, batch_size[1], is_binary)
     
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE_TRAIN, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE_TEST, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size[0], shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size[1], shuffle=False)
     
     N_EPOCHS = 5
     
@@ -165,12 +185,16 @@ if __name__ == "__main__":
     
     init_time = time.time()
     train_loss_history, test_loss_history = [], []
+    train_acc_history, test_acc_history = [], []
+    conf_matrices = []
+    predictions = []
     for epoch in range(1, N_EPOCHS + 1):
         print('Epoch:', epoch)
         start_time = time.time()
-        train(model, optimizer, train_loader, train_loss_history)
+        train_predict = train(model, optimizer, train_loader, train_loss_history, train_acc_history)
         print('Execution time:', '{:5.2f}'.format(time.time() - start_time), 'seconds')
-        evaluate(model, test_loader, test_loss_history)
+        eval_predict = evaluate(model, test_loader, test_loss_history, test_acc_history, conf_matrices, is_binary)
+        predictions.append([train_predict, eval_predict])
         
     minutes, seconds = divmod(time.time() - init_time, 60)
     print('Total execution time:', '{:.0f}m {:.2f}s'.format(minutes, seconds))
@@ -182,10 +206,21 @@ if __name__ == "__main__":
 
 import matplotlib.pyplot as plt
 
-avg_train_loss_history = np.mean(np.array(train_loss_history).reshape(N_EPOCHS,-1), axis=1)
+#avg_train_loss_history = np.mean(np.array(train_loss_history).reshape(N_EPOCHS,-1), axis=1)
 
-plt.plot(avg_train_loss_history)
-plt.plot(test_loss_history, 'r')
+#plt.gca().set_ylim([0,None])
+
+plt.figure()
+plt.plot(train_loss_history, label="train")
+plt.plot(test_loss_history, 'r', label="eval")
+plt.legend(loc="upper right")
+plt.suptitle('Tranformer loss')
+plt.show()
+
+plt.plot(train_acc_history, label="train")
+plt.plot(test_acc_history, 'r', label="eval")
+plt.legend(loc="upper left")
+plt.suptitle('Transformer accuracy')
 plt.show()
 
 
