372353cdaf147a8112a42740fa3f3819c56e80f3
Thijs Werrij <thijswerrij@gmail.com>
2021-03-10 15:31:28 +0100

    Area outside crop is set to minimum if patch is larger than crop; position is now computed from image itself as some info is faulty; load_data can now store crops so they can be easily resized when resizing images

--- load_data.py
+++ load_data.py
@@ -12,7 +12,6 @@ import numpy as np
 import cv2
 import PIL
 import pandas as pd
-import csv
 import h5py
 
 from tqdm import tqdm
@@ -38,10 +37,12 @@ def read_csv(path, sample=None):
     
     labels = data['pathology'].values
     
+    direction = data[['left or right breast', 'image view']].values
+    
     if sample and sample>0:
-        img_paths, labels = img_paths[:sample], labels[:sample]
+        img_paths, labels, direction = img_paths[:sample], labels[:sample], direction[:sample]
                     
-    return img_paths, labels
+    return img_paths, labels, direction
 
 def get_image(path):
     return dcmread(path)
@@ -86,7 +87,7 @@ def to_image(img_array):
 
 def resize_image(img, max_size=0, height=0, width=0, scale=1, keep_ratio=True):
     h, w = img.shape
-    #plot(img)
+    
     if scale != 1:
         return cv2.resize(img, (0, 0), fx=scale, fy=scale, 
                           interpolation=cv2.INTER_CUBIC)
@@ -108,15 +109,6 @@ def resize_image(img, max_size=0, height=0, width=0, scale=1, keep_ratio=True):
     print("Image not resized")
     return img
 
-def get_direction(ds):
-    try:
-        if ds.Laterality in ["L","R"]:
-            return ds.Laterality
-    except:
-        if ds.BodyPartExamined[0] in ["L","R"]:
-            return ds.BodyPartExamined[0]
-    return None
-
 # Credits to Gijs van Tulder
 def compute_patch_offset(centroid, patch_size, image_size):
     centroid = int(np.floor(centroid))
@@ -126,6 +118,7 @@ def compute_patch_offset(centroid, patch_size, image_size):
 
 def get_crops(path_list):
     crops = []
+    bp_list = []
     
     print("Loading images and computing crops...\n")
     sleep(0.2)
@@ -135,43 +128,52 @@ def get_crops(path_list):
         
         pixel_array = ds.pixel_array
         
-        bp = get_direction(ds)
+        # check which side of the image has highest values for direction
+        if np.sum(pixel_array[:,0:5]) > np.sum(pixel_array[:,-5:-1]):
+            bp = 'L'
+        else:
+            bp = 'R'
+        bp_list.append(bp)
         
         cropped = crop_img_from_largest_connected(pixel_array, image_orientation('NO', bp))
         y_min, y_max, x_min, x_max = cropped[0]
-        crops.append((cropped[0], (y_max-y_min,x_max-x_min)))
-    y_max_val = int(np.percentile([y for (_, (y,_)) in crops], 90))
-    x_max_val = int(np.percentile([x for (_, (_,x)) in crops], 90))
-    print("\nMax values:", y_max_val, x_max_val)
+        crops.append([y_min, y_max, x_min, x_max])
     
-    return crops, y_max_val, x_max_val
+    return np.array(crops), bp_list
 
 def get_equal_crops(img, crop, patch_size):
-    #plot(img)
-    y_min, y_max, x_min, x_max = crop[0]
+    y_min, y_max, x_min, x_max = crop
+    min_img_value = np.min(img)
+    
+    # set all values outside of the crop to minimum, so that if the crop is smaller
+    # than the actual patch, outside noise is still reduced
+    img[0:y_min], img[y_max:img.shape[0]], img[:,0:x_min], img[:,x_max:img.shape[1]] = (min_img_value,)*4
     
     y_centroid, x_centroid = y_min+int(np.floor(patch_size[0]/2)), x_min+int(np.floor(patch_size[1]/2))
     
     offsets = [compute_patch_offset(y_centroid, patch_size[0], img.shape[0]),
                compute_patch_offset(x_centroid, patch_size[1], img.shape[1])]
-        
+    
     if patch_size[0] > img.shape[0] or patch_size[1] > img.shape[1]:
         pad_width = ((max(0, patch_size[0] - img.shape[0]), 0),
                          (max(0, patch_size[1] - img.shape[1]), 0))
         offsets = [max(0, offsets[0]), max(0, offsets[1])]
-        img = np.pad(img, pad_width, constant_values=np.min(img))
+        img = np.pad(img, pad_width, constant_values=min_img_value)
         
     patch = img[offsets[0]:offsets[0] + patch_size[0], offsets[1]:offsets[1] + patch_size[1]]
     
     return patch
 
-def get_images_and_resize(path_list, img_scale=1, img_size=None, crops=False, crop_size=False, show_plot=False, include_files=[True,True,True]):
+def get_images_and_resize(path_list, img_scale=1, img_size=None, crops=False, percentile=90, include_files=[True,True,True]):
     image_list = [[],[],[]]
     
     size_is_tuple, size_is_int = type(img_size) is tuple, type(img_size) is int
-    crop_size_is_tuple = type(crop_size) is tuple
     
-    print("Cropping and saving images...\n")
+    if crops is not None:
+        y_max_val = int(np.percentile([y_max-y_min for [y_min,y_max,_,_] in crops], percentile))
+        x_max_val = int(np.percentile([x_max-x_min for [_,_,x_min,x_max] in crops], percentile))
+    
+    print("Preprocessing and saving images...\n")
     sleep(0.3)
     
     for p in tqdm(range(len(path_list))):
@@ -183,9 +185,9 @@ def get_images_and_resize(path_list, img_scale=1, img_size=None, crops=False, cr
                 pixel_array = ds.pixel_array
                 h, w = pixel_array.shape
                 
-                if crops and (i==0 or i==1):
-                    if crop_size_is_tuple and not img_size:
-                        cropped = get_equal_crops(pixel_array, crops[p], crop_size)
+                if crops is not None and (i==0 or i==1):
+                    if not img_size:
+                        cropped = get_equal_crops(pixel_array, crops[p], (y_max_val, x_max_val))
                     else:
                         y_min, y_max, x_min, x_max = crops[p][0]
                         cropped = pixel_array[y_min:y_max,x_min:x_max]
@@ -215,25 +217,55 @@ def get_images_and_resize(path_list, img_scale=1, img_size=None, crops=False, cr
 
 if __name__ == "__main__":
     to_crop = True
+    use_existing_file = True
+    sample = None
+    
     #file_name = "calc_case_description_train_set"
     file_name = "mass_case_description_train_set"
     
-    #path_list, labels = read_csv(f"{file_name}.csv", sample=10)
-    path_list, labels = read_csv(f"{file_name}.csv")
+    path_list, labels, direction = read_csv(f"{file_name}.csv", sample=sample)
+    
+    sample_name = '' if sample is None else f"{sample}_sample_"
     
     if to_crop:
-        crops, y_max, x_max = get_crops(path_list)
+        crops_folder_name = f"{h5_path}crops/"
+        
+        if not os.path.exists(crops_folder_name):
+            os.makedirs(crops_folder_name)
+        
+        crop_filename = f"{crops_folder_name}{sample_name}{file_name}.h5"
+        
+        if use_existing_file:
+            try:
+                with h5py.File(crop_filename, "r+") as f:
+                    crops = np.array(f["/crops"])
+                    bp_list = list(f["/bp"])
+            except:
+                use_existing_file = False
+            
+        if not use_existing_file:
+            crops, bp_list = get_crops(path_list)
+            
+            with h5py.File(crop_filename,'w') as f:
+                crop_set = f.create_dataset(
+                    "crops", crops.shape, data=crops
+                )
+                bp_set = f.create_dataset(
+                    "bp", len(bp_list), data=bp_list
+                )
+            
         
     #%%
     
+    percentile = 90
     img_scale = 1
-    img_scale = 0.2
+    img_scale = 0.1
     img_size = None
     #img_size = (180,180)
     include_files=[True,False,False]
     
     if to_crop:
-        images = get_images_and_resize(path_list, img_scale=img_scale, img_size=img_size, crops=crops, crop_size=(y_max,x_max), include_files=include_files)
+        images = get_images_and_resize(path_list, img_scale=img_scale, img_size=img_size, crops=crops, percentile=percentile, include_files=include_files)
     else:
         images = get_images_and_resize(path_list, img_scale=img_scale, img_size=img_size, include_files=include_files)
     
@@ -256,10 +288,14 @@ if __name__ == "__main__":
     else:
         h5_filename = f"{h5_path}{file_name}_{str_img_size}.h5"
     
-    with h5py.File(h5_filename,'w') as f:
-        dataset = f.create_dataset(
-            "images", saved_images.shape, data=saved_images.astype('float16'), compression='gzip'
-        )
-        meta_set = f.create_dataset(
-            "meta", (labels.shape[0],1), data=labels.reshape(labels.shape[0],1)
-        )
+    if not sample:
+        with h5py.File(h5_filename,'w') as f:
+            dataset = f.create_dataset(
+                "images", saved_images.shape, data=saved_images.astype('float16'), compression='gzip'
+            )
+            meta_set = f.create_dataset(
+                "meta", (labels.shape[0],1), data=labels.reshape(labels.shape[0],1)
+            )
+            bp_set = f.create_dataset(
+                "bp", len(bp_list), data=bp_list
+            )
